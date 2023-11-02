b0fc608d802cd5a21e01091d21a39f343023fffd
sakthivel P <131642753+sakthisakthivel688@users.noreply.github.com>
2023-11-02 11:32:14 +0530

    Add files via upload

---
+++ Task_2/client/src/Components/index.js
@@ -0,0 +1,8 @@
+import Card from "./Card/Card";
+import Nav from "./Navigation/Nav";
+import CatagreeCard from "./Card/CatagreeCard";
+export{
+    Card,
+    Nav,
+    CatagreeCard
+}
---
+++ Task_2/client/src/assets/products/index.js
@@ -0,0 +1,38 @@
+import alanware from './alean-ware-laptop.png'
+import alexa from './alexa-home.png'
+import Electronic from './electronc.png'
+import googleHome from './google-home.png'
+import googleProduct from './google-products.png'
+import googlewatch from './google-watch.png'
+import SamsungS23 from './sm-s23.jpg'
+import sms9 from './sm-s9.png'
+import pixel4a from './pixel4a.jpg'
+import pixel2 from './pixel-2.png'
+import pcwhite from './pc-white.png'
+import pc from './pc.png'
+import Nkband from './nekband.png'
+import moniter from './moniter.png'
+import mobile from './mobile.png'
+import mac from './mac-book.png'
+import meta from  './meta-qust.png'
+import iphone13 from './iphone-13pro-max.png'
+import Btspeaker from './bt-speaker.png'
+export{
+ alexa,
+ alanware,
+ Electronic,
+ googleHome,
+ googleProduct,
+ googlewatch,
+ SamsungS23,
+ sms9,
+ pc,
+ pixel2,
+ pixel4a,
+ pcwhite,
+ Nkband,
+ moniter,
+ mobile,
+ mac,
+ meta,iphone13,Btspeaker
+}
